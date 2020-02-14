5076f8dcb4436867db15326f8a5cf690e7b49717
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-14 13:02:21 +0300

    Add redirect

--- MyStaticFilesLearn/Startup.cs
+++ MyStaticFilesLearn/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace MyStaticFilesLearn
@@ -21,8 +23,12 @@ namespace MyStaticFilesLearn
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDirectoryBrowser();
+            //app.UseDirectoryBrowser();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")), RequestPath = new PathString("/pages")
+            });
             //if (env.IsDevelopment())
             //{
             //    app.UseDeveloperExceptionPage();
